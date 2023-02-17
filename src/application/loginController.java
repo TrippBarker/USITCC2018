5d464a1bf9b120c023b5218885f5468ff7bad8a5
Tripp Barker <colemanbarker@gmail.com>
2023-02-16 21:44:41 -0500

    working on main scene

--- src/application/loginController.java
+++ src/application/loginController.java
@@ -2,7 +2,7 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class loginController {
@@ -10,7 +10,11 @@ public class loginController {
 	@FXML
 	TextField usernameField;
 	
+	@FXML
+	PasswordField passwordField;
+	
 	public void pressButt(ActionEvent e) {
-		System.out.println(usernameField.getText());
+		System.out.println("USER: " + usernameField.getText());
+		System.out.println("PASS: " + passwordField.getText());
 	}
 }
