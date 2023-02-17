64b28b46deeb3b9d80e869badfd4f8629c2e26bb
Tripp Barker <colemanbarker@gmail.com>
2023-02-17 12:40:40 -0500

    working on scenes

---
+++ src/application/LoginController.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+
+public class LoginController {
+	
+	private SceneSwitcher ss = new SceneSwitcher();
+	private String lastTypedUsername = "";
+	
+	@FXML
+	TextField usernameField;
+	
+	@FXML
+	PasswordField passwordField;
+	
+	public void userLogin(ActionEvent e) throws IOException {
+		System.out.println("USER: " + usernameField.getText());
+		System.out.println("PASS: " + passwordField.getText());
+		ss.switchScene(e, "scenes/UserScene.fxml");
+	}
+	
+	public void checkValidChar(KeyEvent ke){
+		System.out.println(ke.getCharacter());
+		if (!Pattern.matches("[A-Za-z\b]", ke.getCharacter()) || (lastTypedUsername.length() == 15 && !ke.getCharacter().equals("\b"))) {
+			usernameField.setText(lastTypedUsername);
+			usernameField.positionCaret(lastTypedUsername.length());
+		} else {
+			lastTypedUsername = usernameField.getText();
+		}
+	}
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,16 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("scenes/LoginScene.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
