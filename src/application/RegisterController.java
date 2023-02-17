1306556b8d9b17e18d808d9f717eb7fffa2fda6c
Tripp Barker <colemanbarker@gmail.com>
2023-02-17 14:52:05 -0500

    working on registration form

--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
@@ -22,10 +26,22 @@ public class RegisterController {
 	ComboBox<String> stateField;
 	
 	@FXML
+	Button backButton, submitButton;
+	
+	@FXML
 	public void initialize() {
 		stateField.getItems().removeAll(stateField.getItems());
 		stateField.getItems().addAll("GA", "TX", "CA");
 	}
 	
+	private SceneSwitcher ss = new SceneSwitcher();
+	
+	public void switchToLoginScene(ActionEvent e) throws IOException {
+		ss.switchScene(e, "scenes/LoginScene.fxml");
+	}
+	
+	public void switchToUserScene(ActionEvent e) throws IOException {
+		ss.switchScene(e, "scenes/UserScene.fxml");
+	}
 	
 }
--- src/application/SceneSwitcher.java
+++ src/application/SceneSwitcher.java
@@ -22,4 +22,6 @@ public class SceneSwitcher {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	
 }
